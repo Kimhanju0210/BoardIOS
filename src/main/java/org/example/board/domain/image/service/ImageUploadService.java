3befd83ea01d6e617ddecfde43e3bdf76213e8b3
김한주 <s24043@gsm.hs.kr>
2025-04-02 17:08:03 +0900

    add :: ImageUploadService.java

---
+++ src/main/java/org/example/board/domain/image/service/ImageUploadService.java
@@ -0,0 +1,7 @@
+package org.example.board.domain.image.service;
+
+import org.example.board.domain.image.entity.Image;
+
+public interface ImageUploadService {
+    Image uploadImage(String contentImage);
+} 
