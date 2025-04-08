4b620b1dbf981d026013cf3aebde2a6d778470b2
김한주 <s24043@gsm.hs.kr>
2025-04-08 21:06:07 +0900

    modify :: ImageUploadServiceImpl.java

--- src/main/java/org/example/board/domain/image/service/impl/ImageUploadServiceImpl.java
+++ src/main/java/org/example/board/domain/image/service/impl/ImageUploadServiceImpl.java
@@ -3,13 +3,18 @@ package org.example.board.domain.image.service.impl;
 import org.example.board.domain.image.entity.Image;
 import org.example.board.domain.image.repository.ImageRepository;
 import org.example.board.domain.image.service.ImageUploadService;
+import org.example.board.global.util.FileUtil;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
 public class ImageUploadServiceImpl implements ImageUploadService {
 
     private final ImageRepository imageRepository;
+    private final String uploadDir = "uploads";
 
     public ImageUploadServiceImpl(ImageRepository imageRepository) {
         this.imageRepository = imageRepository;
@@ -17,10 +22,16 @@ public class ImageUploadServiceImpl implements ImageUploadService {
 
     @Override
     @Transactional
-    public Image uploadImage(String contentImage) {
+    public Image uploadImage(MultipartFile file) throws IOException {
+        String storedFileName = FileUtil.saveFile(file, uploadDir);
+        
         Image image = Image.builder()
-                .contentImage(contentImage)
+                .originalFileName(file.getOriginalFilename())
+                .storedFileName(storedFileName)
+                .filePath(uploadDir + "/" + storedFileName)
+                .fileType(file.getContentType())
                 .build();
+                
         return imageRepository.save(image);
     }
 } 
