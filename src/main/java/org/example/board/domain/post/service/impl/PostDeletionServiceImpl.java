463b2bece91ed50087ec7fa1eb8e91aa62950837
김한주 <s24043@gsm.hs.kr>
2025-04-09 16:56:42 +0900

    modify :: PostDeletionServiceImpl.java

--- src/main/java/org/example/board/domain/post/service/impl/PostDeletionServiceImpl.java
+++ src/main/java/org/example/board/domain/post/service/impl/PostDeletionServiceImpl.java
@@ -1,17 +1,15 @@
 package org.example.board.domain.post.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.example.board.domain.post.repository.PostRepository;
 import org.example.board.domain.post.service.PostDeletionService;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class PostDeletionServiceImpl implements PostDeletionService {
 
     private final PostRepository postRepository;
-
-    public PostDeletionServiceImpl(PostRepository postRepository) {
-        this.postRepository = postRepository;
-    }
 
     @Override
     public void deletePost(Long id) {
